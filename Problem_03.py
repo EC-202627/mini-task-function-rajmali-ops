07944702c6a50d46330dc32196e0256b2127262f
rajmali-ops <raj.mali@avantika.edu.in>
2026-03-28 13:29:31 +0000

    fixing

--- Problem_03.py
+++ Problem_03.py
@@ -1,6 +1,6 @@
 def calculate_fine(book_title, days_overdue, daily_rates = 5.0, max_fine = 150.0):
     fine = days_overdue*daily_rates
-    if fine >= max_fine:
+    if fine == max_fine:
         return "Rs. 150.0 You have accumulated the maximum fine of INR: 150.0"
     else:
         return fine
