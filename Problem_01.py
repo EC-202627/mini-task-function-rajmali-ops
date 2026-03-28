f8b2d6ba2bef9043a987576c34557dfd46218c44
rajmali-ops <raj.mali@avantika.edu.in>
2026-03-28 12:15:31 +0000

    Fixing

--- Problem_01.py
+++ Problem_01.py
@@ -1,8 +1,10 @@
 def calculate_fine(book_title, days_overdue, daily_rates = 5.0):
     fine = days_overdue*daily_rates
-    return f"Book: {book_title} Days overdue: {days_overdue} Fine: Rs. {fine}"
+    return fine
 
 book_title = input()
 days_overdue = int(input())
 fine = calculate_fine(book_title, days_overdue)
-print(fine)
+print("Book:", book_title)
+print("Days overdue:", days_overdue)
+print("Fine: Rs.", fine)
