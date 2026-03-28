6b6ff8756757e32c6f394ea4ffda67cc36ff393d
rajmali-ops <raj.mali@avantika.edu.in>
2026-03-28 13:05:00 +0000

    fixing

--- Problem_03.py
+++ Problem_03.py
@@ -1,13 +1,14 @@
 def calculate_fine(book_title, days_overdue, daily_rates = 5.0, max_fine = 150.0):
     fine = days_overdue*daily_rates
     if fine >= max_fine:
-        return f" {max_fine} You have accumulated the maximum fine of INR: 150.0"
+        return f"{max_fine} You have accumulated the maximum fine of INR: 150.0"
     else:
         return fine
 
 book_title = input()
 days_overdue = int(input())
-fine = calculate_fine(book_title, days_overdue)
+daily_rates = int(input())
+fine = calculate_fine(book_title, days_overdue,daily_rates)
 print("Book:", book_title)
 print("Days overdue:", days_overdue)
 print("Fine: Rs.", fine)
